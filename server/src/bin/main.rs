4076a3d1b28e5658a661279c1215192355a882d2
Joe Thornton <joe.clifford.thornton@gmail.com>
2022-05-18 20:59:05 +0100

    Added ability for graceful shutdown.

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -1,3 +1,4 @@
+use server::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
@@ -5,20 +6,19 @@ use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
-use server::ThreadPool;
-
 fn main() {
-    let listener: TcpListener = TcpListener::bind("localhost:7878").unwrap();
-
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-        let stream: TcpStream = stream.unwrap();
+        let stream = stream.unwrap();
 
         pool.execute(|| {
-            handle_connection(stream)
+            handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -28,20 +28,21 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, filename) =
-        if  buffer.starts_with(get) {
-            ("HTTP/1.1 200 OK", "index.html")
-        } else if buffer.starts_with(sleep){
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "index.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
-        };
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
     let contents = fs::read_to_string(filename).unwrap();
 
-    let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}",
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
         status_line,
-        contents.len(), 
+        contents.len(),
         contents
     );
 
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,20 +1,28 @@
-use std::{thread::{Thread, self}, sync::{mpsc, Mutex, Arc}};
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
-impl ThreadPool{
-    /// Create a new ThreadPool
-    /// 
-    /// The Size is the number of threads in the pool
-    /// 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
     /// # Panics
-    /// 
-    /// The 'new' function will panic if the size is zero.
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
@@ -25,10 +33,7 @@ impl ThreadPool{
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(
-                id, 
-                Arc::clone(&receiver)
-            ));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool { workers, sender }
@@ -36,32 +41,61 @@ impl ThreadPool{
 
     pub fn execute<F>(&self, f: F)
     where
-        F: FnOnce() + Send + 'static
-        {
-            let job = Box::new(f);
-            self.sender.send(job).unwrap();
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
         }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver
-                .lock()
-                .unwrap()
-                .recv()
-                .unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-                println!("Worker {} get a job; executing.", id);
-                
-                job();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+
+                    break;
+                }
+            }
         });
 
-        Worker {id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
