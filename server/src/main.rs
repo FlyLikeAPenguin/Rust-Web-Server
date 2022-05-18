603dd9ffdfe3aabcaa0909da6bf384c55d6121d7
Joe Thornton <joe.clifford.thornton@gmail.com>
2022-05-18 18:38:19 +0100

    Dumps requests to the console.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,19 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main() {
     let listener: TcpListener = TcpListener::bind("localhost:7878").unwrap();
 
-    for stream in listener.incoming(){
+    for stream in listener.incoming() {
         let stream: TcpStream = stream.unwrap();
-        
-        println!("Connection established!");
+        handle_connection(stream)
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
 }
