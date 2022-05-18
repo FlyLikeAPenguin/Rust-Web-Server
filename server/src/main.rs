e290d7054f0569c9f68dd5346d96f2dd8ed7aeed
Joe Thornton <joe.clifford.thornton@gmail.com>
2022-05-18 18:40:43 +0100

    Sends a valid 200 response

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,6 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
 
 fn main() {
     let listener: TcpListener = TcpListener::bind("localhost:7878").unwrap();
@@ -13,7 +13,9 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    
+
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]))
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
